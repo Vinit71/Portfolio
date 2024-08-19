1bf260e83e3dd40431368f5db77a8fcdd06dc369
Vinit <vinitsk99@gmail.com>
2024-08-19 14:58:37 +0530

    Add responsiveness

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,12 +3,13 @@ function Footer() {
     <div className='mx-4 sm:mx-8 md:mx-12 my-8 flex flex-col gap-7 py-10'>
         <hr />
         <div className="flex flex-col sm:flex-row justify-between text-base sm:text-lg">
-            <p className="max-w-full sm:max-w-md mb-4 sm:mb-0">Lorem, ipsum dolor sit amet consectetur adipisicing.</p>
-            <div className="flex flex-col sm:flex-row gap-4 sm:gap-8 md:gap-12 mb-4 sm:mb-0">
-                <p>Lorem, ipsum dolor.</p>
-                <p>Lorem, ipsum dolor.</p>
-                <p>Lorem, ipsum.</p>
+            <p className="text-center max-w-full sm:max-w-md mb-4 sm:mb-0">Focused on providing practical solutions and maintaining high standards in every project.</p>
+            
+            <div className="flex justify-between mx-10 sm:flex-row sm:justify-between gap-4 sm:gap-8 md:gap-12 mb-4 sm:mb-0">
+              <p className="">Terms and Services</p>
+              <p className="">Contact me</p>
             </div>
+
         </div>
     </div>
 
